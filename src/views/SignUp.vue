cc83b76157139e1f543ce5a594c1afc8f1ce64b1
jason60810 <jason60810@gmail.com>
2022-03-27 18:25:30 +0800

    P7

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -3,8 +3,14 @@
     <v-row>
       <v-col>
         <h1>Signup</h1>
-        <v-form>
-          <v-text-field type="email" label="Email"></v-text-field>
+        <v-form ref="signUpForm" v-model="formValidity">
+          <v-text-field
+            type="email"
+            label="Email"
+            v-model="email"
+            required
+            :rules="emailRules"
+          ></v-text-field>
 
           <v-autocomplete
             label="Which browser do you use?"
@@ -21,37 +27,30 @@
 
           <v-date-picker v-model="birthday"></v-date-picker>
 
-          <v-menu
-            ref="menu"
-            v-model="menu"
-            :close-on-content-click="false"
-            :return-value.sync="date"
-            transition="scale-transition"
-            offset-y
-            min-width="auto"
+          <v-checkbox
+            label="Agree to terms & conditions"
+            v-model="agreeToTerms"
+            required
+            :rules="agreeToTermsRules"
+          ></v-checkbox>
+
+          <v-btn
+            :disabled="!formValidity"
+            class="mr-4"
+            type="submit"
+            color=" primary"
+            >Submit</v-btn
           >
-            <template v-slot:activator="{ on, attrs }">
-              <v-text-field
-                v-model="date"
-                label="Picker in menu"
-                prepend-icon="mdi-calendar"
-                readonly
-                v-bind="attrs"
-                v-on="on"
-              ></v-text-field>
-            </template>
-            <v-date-picker v-model="date" no-title scrollable>
-              <v-spacer></v-spacer>
-              <v-btn text color="primary" @click="menu = false"> Cancel </v-btn>
-              <v-btn text color="primary" @click="$refs.menu.save(date)">
-                OK
-              </v-btn>
-            </v-date-picker>
-          </v-menu>
 
-          <v-checkbox label="Agree to terms & conditions"></v-checkbox>
+          <v-btn class="mr-4" color="success" @click="validateForm"
+            >Validate Form</v-btn
+          >
 
-          <v-btn type="submit" color=" primary">Submit</v-btn>
+          <v-btn class="mr-4" color="warning" @click="resetValidation"
+            >Reset Validation</v-btn
+          >
+
+          <v-btn color="error" @click="resetForm">Reset</v-btn>
         </v-form>
       </v-col>
     </v-row></v-container
@@ -62,14 +61,41 @@
 export default {
   data() {
     return {
-      browsers: ['Chrome', 'Firefox', 'Safari', 'Edge', 'Brave'],
+      agreeToTerms: false,
+      agreeToTermsRules: [
+        (value) =>
+          !!value ||
+          'You must agree to the terms and conditions to sign up for an account.',
+      ],
       birthday: '',
-      date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
-        .toISOString()
-        .substr(0, 10),
-      menu: false,
-      modal: false,
+      browsers: ['Chrome', 'Firefox', 'Safari', 'Edge', 'Brave'],
+      email: '',
+      emailRules: [
+        (value) => !!value || 'Email is required.',
+        (value) => value.indexOf('@') !== 0 || 'Email should have a username.',
+        (value) => value.includes('@') || 'Email should include an @ symbol.',
+        (value) =>
+          value.indexOf('.') - value.indexOf('@') > 1 ||
+          'Email should contain a valid domain.',
+        (value) =>
+          value.includes('.') || 'Email should include a period symbol.',
+        (value) =>
+          value.indexOf('.') <= value.length - 3 ||
+          'Email should contain a valid domain extension.',
+      ],
+      formValidity: false,
     };
+  },
+  methods: {
+    resetForm() {
+      this.$refs.signUpForm.reset();
+    },
+    resetValidation() {
+      this.$refs.signUpForm.resetValidation();
+    },
+    validateForm() {
+      this.$refs.signUpForm.validate();
+    },
   },
 };
 </script>
